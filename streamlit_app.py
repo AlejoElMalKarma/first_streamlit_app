951db1db07a08c0d1bdf79a8e7076db623febf2f
AlejoElMalKarma <130944145+AlejoElMalKarma@users.noreply.github.com>
2023-11-08 16:10:13 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,5 +52,5 @@ my_fruit_list_2 = my_data_rows
 
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-fruits_selected_list = streamlit.select("What fruit would yo like to add?", list(my_data_rows.index))
+fruits_selected_list = streamlit.multiselect("What fruit would yo like to add?", list(my_data_rows.index))
 fruits_to_show_2 = my_fruit_list_2.loc[fruits_selected_list]
